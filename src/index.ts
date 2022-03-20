e43beabe046ff9f60e1ad7699ddd0e23aa24975e
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-03-21 01:04:39 +0200

    parametrize the custom-map constructor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
+import { CustomMap } from './CustomMap';
 import { User } from "./User";
 const user= new User();
 console.log(user);
 
-new google.maps.Map(document.getElementById("map"),{
-    zoom:1,
-    center:{
-        lat:0,
-        lng:0
-    }
-});
+new CustomMap('map');
